279bfd9331588c9d82f3e8589bc67eaa74b615ce
unknown <anterebol@gmail.com>
2022-06-15 15:52:50 +0300

    feat: add solution and started tests

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,121 @@
 import fsPromise from 'fs/promises';
+const uuid = require('uuid');
 import path from 'path';
 import http from 'http';
-import url from 'url';
 import Emmit from 'events';
-import { idText } from 'typescript';
+import dotenv from 'dotenv';
 let dataBase = require('./data.json');
 
-const createId = (count: number) => {
-  const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  let id = 'id_';
+dotenv.config();
+const PORT = process.env.PORT
 
-  for (let i = 0; i < count; i++) {
-    const item = Math.floor(Math.random() * 36);
-    id += arr[item];
-  }
-  return id;
+function checkUuid(id: string) {
+  return /[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}/.exec(id);
 }
-function updateDB(data, code, res, user) {
+
+export function createId() {
+  return uuid.v4();
+}
+
+function updateDB(data: JSON, code: number, res: any, user: {}, status) {
   fsPromise.writeFile(path.join(__dirname, 'data.json'), JSON.stringify(data, null, 2)).then(() => {
+    const data = {
+      message: 'User ' + status,
+      user: {
+        ...user
+      }
+    }
     res.writeHead(code, 'ok', {'content-type': 'application/json'});
-    res.write(JSON.stringify(user));
+    res.write(JSON.stringify(data));
     res.end();
   }).catch((err) => {
     res.writeHead(500, 'Server error', {'content-type': 'application/json'});
     res.end();
   })
 }
-const server = http.createServer(async (req, res) => {
-  let urlReq = url.parse(req.url, true);
+
+export const server = http.createServer(async (req, res) => {
   let answer: string;
   const urlParse = req.url.split(/\//g).filter(item => item !== '')
   const userId = urlParse.length === 3 ? urlParse[2] : '';
   let userInfo = Object.create({});
   switch (true) {
     case req.url === '/api/users' && req.method === 'GET':
-      res.writeHead( 200, 'Ok', {'content-type' : 'JSON'});
-      res.write(JSON.stringify(dataBase));
-      res.end()
+      try {
+        res.writeHead( 200, 'Ok', {'content-type' : 'JSON'});
+        res.write(JSON.stringify(dataBase));
+        res.end();
+      } catch {
+        res.writeHead( 500, 'Server error', {'content-type' : 'text/plain'});
+        res.end();
+      }
       break;
     case req.url === `/api/users/${userId}` &&  req.method === 'GET':
-      if (/id_[0-9a-z]{4}/.exec(userId)) {
-        for (let id in dataBase) {
-          if (id === userId) {
-            answer = JSON.stringify(dataBase[id]);
-            res.writeHead( 201, 'Ok', {'content-type' : 'JSON'});
-            res.write('User info: ' + answer);
+      try {
+        if (checkUuid(userId) && userId.length === 36) {
+          if (dataBase[userId]) {
+            answer = JSON.stringify(dataBase[userId]);
+            res.writeHead( 200, 'Ok', {'content-type' : 'aplication/json'});
+            res.write(answer);
             res.end();
           }
-        }
-        if (!answer) {
-          res.writeHead( 404, 'User not found', {'content-type' : 'text/plain'});
+          if (!answer) {
+            res.writeHead( 404, 'User not found', {'content-type' : 'text/plain'});
+            res.end();
+          }
+        } else {
+          res.writeHead( 400, 'Wrong Id', {'content-type' : 'text/plain'});
           res.end();
         }
-      } else {
-        res.writeHead( 400, 'Wrong Id', {'content-type' : 'text/plain'});
+      } catch {
+        res.writeHead( 500, 'Server error', {'content-type' : 'text/plain'});
         res.end();
       }
       break;
     case req.url === `/api/users` &&  req.method === 'POST':
-      const postEmmiter = new Emmit();
-      postEmmiter.on('post_emmit', () => {
-        const { username, age, hobbies } = userInfo;
-        if (username && age && hobbies) {
-          let id = createId(4);
-          while (dataBase[id]) {
-            id = createId(4);
+      try {
+        const postEmmiter = new Emmit();
+        postEmmiter.on('post_emmit', () => {
+          const { username, age, hobbies } = userInfo;
+          if (username && age && hobbies) {
+            let id = createId();
+            while (dataBase[id]) {
+              id = createId();
+            }
+            const user = { id, username, age, hobbies }
+            dataBase[id] = { ...user };
+            updateDB(dataBase, 201, res, user, 'added')
+          } else {
+            res.writeHead(404, 'Bad Request, body is invalid', {'content-type': 'application/json'});
+            res.end();
           }
-          const user = { id, username, age, hobbies }
-          dataBase[id] = { ...user };
-          updateDB(dataBase, 201, res, user)
-        } else {
-          res.writeHead(404, 'Bad Request, body is invalid', {'content-type': 'application/json'});
-          res.end();
-        }
-      });
-      req.on('data', async (data) => {
-        const objData = await JSON.parse(data);
-        for (let key in objData) {
-          userInfo[key] = objData[key];
-        }
-      });
-      req.on('end', () => {
-        postEmmiter.emit('post_emmit');
-      })
+        });
+        req.on('data', async (data) => {
+          const objData = await JSON.parse(data);
+          for (let key in objData) {
+            userInfo[key] = objData[key];
+          }
+        });
+        req.on('end', () => {
+          postEmmiter.emit('post_emmit');
+        })
+      } catch {
+        res.writeHead( 500, 'Server error', {'content-type' : 'text/plain'});
+        res.end();
+      }
       break;
     case req.url === `/api/users/${userId}` &&  req.method === 'PUT':
-        if (/id_[0-9a-z]{4}/.exec(userId)) {
-          for (let id in dataBase) {
-            if (id === userId) {
+        try {
+          if (checkUuid(userId) && userId.length === 36) {
+            if (dataBase[userId]) {
               const putEmmiter = new Emmit();
               putEmmiter.on('put_emmit', () => {
                 const username = userInfo.username || dataBase[userId].username;
                 const age = userInfo.age || dataBase[userId].age
                 const hobbies = userInfo.hobbies || dataBase[userId].hobbies;
-                const user = { userId, username, age, hobbies }
+                const user = { id: userId, username, age, hobbies }
                 dataBase[userId] = { ...user };
-                updateDB(dataBase, 200, res, user);
+                updateDB(dataBase, 200, res, user, 'Update');
               });
               req.on('data', (data) => {
                 const objData = JSON.parse(data);
@@ -107,12 +127,28 @@ const server = http.createServer(async (req, res) => {
                 putEmmiter.emit('put_emmit');
               })
             }
+          } else {
+              res.writeHead( 400, 'Wrong Id', {'content-type' : 'text/plain'});
+              res.end();
           }
-        } else {
-            res.writeHead( 400, 'Wrong Id', {'content-type' : 'text/plain'});
-            res.end();
+        } catch {
+          res.writeHead( 500, 'Server error', {'content-type' : 'text/plain'});
+          res.end();
         }
-        break;
+      break;
+    case req.url === `/api/users/${userId}` &&  req.method === 'DELETE':
+      if (checkUuid(userId) && userId.length === 36) {
+        if (dataBase[userId]) {
+          const { username, age, hobbies } = dataBase[userId];
+          const user = { id: userId, username, age, hobbies }
+          delete dataBase[userId];
+          updateDB(dataBase, 200, res, user, 'Delete');
+        }
+      } else {
+        res.writeHead( 400, 'Wrong Id', {'content-type' : 'text/plain'});
+        res.end();
+      }
+      break;
     default:
       res.writeHead( 404, 'This page not found', {'content-type' : 'text/plain'});
       res.end();
@@ -120,4 +156,5 @@ const server = http.createServer(async (req, res) => {
   }
 
 })
-server.listen(4000);
+server.listen(PORT);
+server;
